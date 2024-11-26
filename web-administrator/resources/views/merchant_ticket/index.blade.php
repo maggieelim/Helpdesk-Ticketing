7ed71476e837c976c696ff7ae2e6b48648147df5
maggieelim <maggie.535210017@stu.untar.ac.id>
2024-11-26 20:26:37 +0700

    test

--- web-administrator/resources/views/merchant_ticket/index.blade.php
+++ web-administrator/resources/views/merchant_ticket/index.blade.php
@@ -8,7 +8,7 @@
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item active">Ticket test</li>
+          <li class="breadcrumb-item active">Ticket</li>
         </ol>
       </div>
     </div>
